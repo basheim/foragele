0a98516177785c8e7feb79ba7af6ca8fd32f83d9
Brandon Asheim <brandonasheim@gmail.com>
2022-10-25 11:58:28 -0700

    fix(): move to python 3 interpretor

--- scripts/startup_ec2.sh
+++ scripts/startup_ec2.sh
@@ -7,4 +7,4 @@ npm run build
 pm2 kill
 pm2 --name foragele start npm -- start
 pm2 start ~/beans-backend/scripts/keep_db_open.sh --cron "0 * * * *"
-pm2 start ~/beans-backend/scripts/python/plants/plant_refresh.py --cron "0 * * * *" -- -s "~/beans-backend/scripts/python/plants/plant_replace.py" -i "~/beans-backend/scripts/python/plants/data/plant_data.csv" -j "~/beans-backend/scripts/python/plants/data/images"
+pm2 start ~/beans-backend/scripts/python/plants/plant_refresh.py --cron "0 * * * *" --interpreter python3 -- -s "~/beans-backend/scripts/python/plants/plant_replace.py" -i "~/beans-backend/scripts/python/plants/data/plant_data.csv" -j "~/beans-backend/scripts/python/plants/data/images"
