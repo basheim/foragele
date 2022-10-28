8514fc2c43ed5fa47c5efe74ffff751237b81210
Brandon Asheim <brandonasheim@gmail.com>
2022-10-28 10:32:33 -0700

    feat(): update the welcome page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,31 @@
 import Head from 'next/head';
 import Script from 'next/script';
 import Hor from '../components/layout/hor';
+import RowItem from '../components/layout/row-item';
 import TextItem from '../components/layout/text-item';
 import Vert from '../components/layout/vert';
+import { PreviewData } from '../lib/interfaces';
 import styles from '../styles/Home.module.css';
 
+export interface HomeProps {
+  previews: PreviewData[];
+}
 
-const Home = () => {
+const Home = ({ previews } : HomeProps) => {
+
+  const LIMIT = 4;
+
+  const getPreviews = (limit?: number) => {
+    const sortedPreviews = previews.sort((a,b) => (new Date(b.createdDate)).getTime() - (new Date(a.createdDate)).getTime());
+    const htmlList = [];
+    for (const preview of sortedPreviews) {
+      if (limit !== undefined && htmlList.length >= limit) {
+        break;
+      }
+      htmlList.push(<RowItem key={preview.id} title={preview.title} miniText={preview.description} urlPath={`/blog/${preview.id}`}/>)
+    }
+    return htmlList;
+  };
 
   return (
     <div className={styles.container}>
@@ -23,14 +42,30 @@ const Home = () => {
         <link rel="manifest" href="/main/site.webmanifest" />
       </Head>
       <main className={styles.main}>
+        <Vert>
+          <h1>Welcome</h1>
+        </Vert>
         <Hor>
           <Vert fullScreen>
-            <TextItem title="The Beginning" body="This is just the beginning of the wedsite. More games, blog posts, and general programming content will be added soon."/>
+            <h3>Suggested Articles</h3>
+            <Hor>
+              {getPreviews(LIMIT)}
+            </Hor>
+            <h3>Suggested Games</h3>
+            <Hor>
+              <RowItem key="foragele" title="Foragele" miniText="Learn more about wild plants and fungi." urlPath="/foragele"/>
+            </Hor>
           </Vert>
         </Hor>
       </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`https://backend.programmingbean.com/api/v1/previews`);
+  const previews = await res.json() as PreviewData[];
+  return { props: { previews } }
 }
 
 export default Home
