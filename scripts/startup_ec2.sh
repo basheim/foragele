d4fa323df0fbdc6d022a036e7738086f749f3338
Brandon Asheim <brandonasheim@gmail.com>
2023-01-23 12:46:18 -0800

    fix(): checkout sitemap

--- scripts/startup_ec2.sh
+++ scripts/startup_ec2.sh
@@ -8,3 +8,4 @@ pm2 kill
 pm2 --name foragele start npm -- start
 pm2 start ~/beans-backend/scripts/keep_db_open.sh --cron "0 * * * *"
 pm2 start ~/beans-backend/scripts/update_plants.sh --cron "0 * * * *"
+git checkout .
